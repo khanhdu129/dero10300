278e668bb4a40e47126b5b79aa7be743280cc216
khanhdu129 <126155375+khanhdu129@users.noreply.github.com>
2023-02-28 21:04:08 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo
 while :; do
-    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjc9k2q8rzy43 -r  dero-node-sg.mysrv.cloud:10300 -r1 community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjc9k2q8rzy43 -r  dero-node-sg.mysrv.cloud:10300 -r1 community-pools.mysrv.cloud:10300 -m 8 -p rpc;
     sleep 5;
 done
